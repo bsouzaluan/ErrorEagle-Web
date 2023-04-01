a9668ef6d8bc57af3f4f3654f9c1af514e2a3168
fernandeess <matheus.rodrigues@sptech.school>
2023-04-01 13:58:20 -0300

    validçoes e masks do cadastro funcinando

--- web-data-viz/site/public/js/funcoesIndex.js
+++ web-data-viz/site/public/js/funcoesIndex.js
@@ -21,77 +21,137 @@ function closeLogin() {
     loginId.style = "display:none;"
 
 }
-function btnSairShow() {
-    if (sessionStorage.length == 0) {
-        console.log(sessionStorage.lenght)
-        document.getElementById("sairBtn").style = "display:none;"
-    } else {
-        console.log(sessionStorage.lenght)
-        document.getElementById("sairBtn").style = "display: inline-block;"
-    }
-}
+// function btnSairShow() {
+//     if (sessionStorage.length == 0) {
+//         console.log(sessionStorage.lenght)
+//         document.getElementById("sairBtn").style = "display:none;"
+//     } else {
+//         console.log(sessionStorage.lenght)
+//         document.getElementById("sairBtn").style = "display: inline-block;"
+//     }
+// }
 function validarCadastro() {
     alert(`Por favor, insira os dados corretamente.`)
 
     nomeVar = nomeCadastro.value;
+    razaoSocial = inpRazaoSocial.value;
+    cnpj = inpCNPJ.value;
+    telefone = inpTelefone.value;
+    telefone1 = inpTelefone1.value;
     emailVar = inpEmailCadastro.value;
     senhaVar = inpSenhaCadastro.value;
     confirmacaoSenhaVar = inpSenhaConfirmacao.value;
-    codigoEmpresaVar = inpCodigoEmpresa.value;
 
-
-    if (nomeVar == "") {
-
+    if (nomeVar == "" ||
+        razaoSocial == "" ||
+        cnpj == "" ||
+        telefone == "" ||
+        telefone1 == "" ||
+        emailVar == "" ||
+        senhaVar == "" ||
+        confirmacaoSenhaVar == ""
+    ) {
         nomeCadastro.style = " border: 3px solid #ff0000 ;"
+        inpRazaoSocial.style = " border: 3px solid #ff0000 ;"
+        inpCNPJ.style = " border: 3px solid #ff0000 ;"
+        inpTelefone.style = " border: 3px solid #ff0000 ;"
+        inpTelefone1.style = " border: 3px solid #ff0000 ;"
+        inpEmailCadastro.style = " border: 3px solid #ff0000 ;"
+        inpSenhaCadastro.style = " border: 3px solid #ff0000 ;"
+        inpSenhaConfirmacao.style = " border: 3px solid #ff0000 ;"
+        return false;
 
     } else {
         nomeCadastro.style = "border: 1px solid #ccc; "
 
     }
 
+    if (nomeVar == "") {
+        nomeCadastro.style = " border: 3px solid #ff0000 ;"
+        return false;
+
+    } else {
+        nomeCadastro.style = "border: 1px solid #ccc; "
+
+    }
+
+    if (razaoSocial == "") {
+        inpRazaoSocial.style = " border: 3px solid #ff0000 ;"
+        return false;
+
+    } else {
+        inpRazaoSocial.style = "border: 1px solid #ccc; "
+
+    }
+    if (cnpj == "") {
+        inpCNPJ.style = " border: 3px solid #ff0000 ;"
+        return false;
+
+    } else {
+        inpCNPJ.style = "border: 1px solid #ccc; "
+
+    }
+
+    if (telefone == "") {
+        inpTelefone.style = " border: 3px solid #ff0000 ;"
+        return false;
+
+    } else {
+        inpTelefone.style = "border: 1px solid #ccc; "
+
+    }
+
+    if (telefone1 == "") {
+        inpTelefone1.style = " border: 3px solid #ff0000 ;"
+        return false;
+
+    } else {
+        inpTelefone1.style = "border: 1px solid #ccc; "
+
+    }
+
     if (emailVar == "") {
 
         inpEmailCadastro.style = " border: 3px solid #ff0000 ;"
+        return false;
 
     } else {
         inpEmailCadastro.style = " border: 1px solid #ccc;  "
+
     }
 
     if (senhaVar == "") {
 
         inpSenhaCadastro.style = " border: 3px solid #ff0000 ;"
-
+        return false;
     }
+
     else {
         inpSenhaCadastro.style = "border: 1px solid #ccc;"
-    }
-
-    if (confirmacaoSenhaVar == "") {
-
-        inpSenhaConfirmacao.style = " border: 3px solid #ff0000 ;"
 
     }
-    else {
-        inpSenhaConfirmacao.style = "  border: 1px solid #ccc; "
-    }
 
-    if (codigoEmpresaVar == "") {
 
-        inpCodigoEmpresa.style = " border: 3px solid #ff0000 ;"
 
-    } else {
-        inpCodigoEmpresa.style = " border: 1px solid #ccc; "
-    }
-    console.log(senhaVar + " " + confirmacaoSenhaVar)
-    if (senhaVar != confirmacaoSenhaVar) {
+}
+function limparErros() {
+    nomeVar = nomeCadastro.value;
+    razaoSocial = inpRazaoSocial.value;
+    cnpj = inpCNPJ.value;
+    telefone = inpTelefone.value;
+    telefone1 = inpTelefone1.value;
+    emailVar = inpEmailCadastro.value;
+    senhaVar = inpSenhaCadastro.value;
+    confirmacaoSenhaVar = inpSenhaConfirmacao.value;
 
-        alert(`As senhas não coincidem!`)
-        inpSenhaConfirmacao.style = " border: 3px solid #ff0000 ;"
-        inpSenhaCadastro.style = " border: 3px solid #ff0000 ;"
-
-    } else {
-        console.log("Senha correta")
-    }
+    nomeCadastro.style = " border: 3px solid #ccc; ;"
+    inpRazaoSocial.style = " border: 3px solid #ccc; ;"
+    inpCNPJ.style = " border: 3px solid #ccc; ;"
+    inpTelefone.style = " border: 3px solid #ccc; ;"
+    inpTelefone1.style = " border: 3px solid #ccc; ;"
+    inpEmailCadastro.style = " border: 3px solid #ccc; ;"
+    inpSenhaCadastro.style = " border: 3px solid #ccc; ;"
+    inpSenhaConfirmacao.style = " border: 3px solid #ccc; ;"
 }
 function validarLogin() {
 
@@ -119,6 +179,7 @@ function validarLogin() {
 
 }
 function entrar() {
+    console.log("Entrei na function entrar")
     // aguardar();
 
 
@@ -189,64 +250,72 @@ function cadastrar() {
     // Agora vá para o método fetch logo abaixo
     var nomeVar = nomeCadastro.value;
     var emailVar = inpEmailCadastro.value;
-    codigoEmpresaVar = inpCodigoEmpresa.value;
     var senhaVar = inpSenhaCadastro.value;
     var confirmacaoSenhaVar = inpSenhaConfirmacao.value;
 
-    if (
-        nomeVar == "" ||
-        emailVar == "" ||
-        senhaVar == "" ||
-        confirmacaoSenhaVar == "" ||
-        codigoEmpresaVar == ""
-    ) {
-        console.log("Campos vazios");
+
+
+
+    if (validarCadastro() == true) {
+        fetch("/usuarios/cadastrar", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                // crie um atributo que recebe o valor recuperado aqui
+                // Agora vá para o arquivo routes/usuario.js
+                nomeServer: nomeVar,
+                emailServer: emailVar,
+                senhaServer: senhaVar,
+            }),
+        })
+            .then(function (resposta) {
+                console.log("resposta: ", resposta);
+
+                if (resposta.ok) {
+                    // cardErro.style.display = "block";
+
+                    //mensagem_erro.innerHTML =
+                    // "Cadastro realizado com sucesso! Redirecionando para tela de Login...";
+
+                    setTimeout(() => {
+                        window.location = "index.html";
+                    }, "2000");
+
+                    // limparFormulario();
+                    // finalizarAguardar();
+                } else {
+                    throw "Houve um erro ao tentar realizar o cadastro!";
+                }
+            })
+            .catch(function (resposta) {
+                console.log(`#ERRO: ${resposta}`);
+                // finalizarAguardar();
+            });
 
         return false;
-    } else {
-        validarCadastro();
     }
 
     // Enviando o valor da nova input
-    fetch("/usuarios/cadastrar", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            // crie um atributo que recebe o valor recuperado aqui
-            // Agora vá para o arquivo routes/usuario.js
-            nomeServer: nomeVar,
-            emailServer: emailVar,
-            senhaServer: senhaVar,
-        }),
-    })
-        .then(function (resposta) {
-            console.log("resposta: ", resposta);
 
-            if (resposta.ok) {
-                // cardErro.style.display = "block";
-
-                //mensagem_erro.innerHTML =
-                // "Cadastro realizado com sucesso! Redirecionando para tela de Login...";
-
-                setTimeout(() => {
-                    window.location = "index.html";
-                }, "2000");
-
-                // limparFormulario();
-                // finalizarAguardar();
-            } else {
-                throw "Houve um erro ao tentar realizar o cadastro!";
-            }
-        })
-        .catch(function (resposta) {
-            console.log(`#ERRO: ${resposta}`);
-            // finalizarAguardar();
-        });
-
-    return false;
 }
 
-btnSairShow();
+// btnSairShow();
 
+//Mask Test
+//CleaverJS lib de masks
+new Cleave('.input-phone', {
+    delimiters: ['(', ') ', ' - '],
+    blocks: [0, 2, 5, 4]
+});
+new Cleave('.input-phone1', {
+    delimiters: ['(', ') ', ' - '],
+    blocks: [0, 2, 5, 4]
+});
+
+var cleave = new Cleave('.cnpjMask', {
+    delimiters: ['.', '.', '/', '-'],
+    blocks: [2, 3, 3, 4, 2],
+
+});
