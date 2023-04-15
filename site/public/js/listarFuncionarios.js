68a9552d8b68eae79c3d6cfc6de23b39ed321dad
fernandeess <matheus.rodrigues@sptech.school>
2023-04-15 13:55:34 -0300

    feature:bolaboradores sendo listados

--- site/public/js/listarFuncionarios.js
+++ site/public/js/listarFuncionarios.js
@@ -1,47 +1,155 @@
 
-var empresa = sessionStorage.FK_EMPRESA;
-var funcionarios;
-var listaDiv = document.getElementById("listaItens")
-fetch(`/supervisor/listarFuncionarios/${empresa}`, {
-    method: "GET",
-    headers: {
-        "Content-Type": "application/json",
-    },
-})
-    .then(function (resposta) {
-        console.log("ESTOU NO THEN DO listar()!");
+function listarFuncionarios() {
+    var empresa = sessionStorage.FK_EMPRESA;
+    var funcionarios;
 
-        if (resposta.ok) {
-            resposta.json().then((json) => {
-                console.log(json);
-                funcionarios = json;
-                console.log("Funcionarios: ", funcionarios);
-                for (var i = 0; i < funcionarios.length;) {
+    fetch(`/gerenciadorUsuario/listarFuncionarios/${empresa}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then(function (resposta) {
+            console.log("ESTOU NO THEN DO listar()!");
 
-                    if (funcionarios[i].fkSupervisor == null) {
+            if (resposta.ok) {
+                resposta.json().then((json) => {
 
-                    } else {
-                        listaDiv.innerHTML += ` <div>
-                        ${funcionarios[i].nome}
-                        ${funcionarios[i].email}
-                        ${funcionarios[i].fkSupervisor}
-                        </div> `
+                    funcionarios = json;
+                    for (var i = 0; i < funcionarios.length;) {
+
+                        if (funcionarios[i].fkSupervisor == null) {
+
+
+
+                        } else {
+
+                            listaAtivosDiv.innerHTML += `<div class="item"> <span class="nomeItemLista">Nome: ${funcionarios[i].nome}</span> <span class="emailItemLista">Email: ${funcionarios[i].email}</span> <span
+                        class="telefoneItemLista"> Telefone: ${funcionarios[i].telefone}  </span>
+                         
+                    <button class="btnDelete" onclick="deleteFuncionario(${funcionarios[i].idFuncionario})">Remover Acesso</button>`
+                        }
+                        i++;
                     }
-                    i++;
-                }
-            });
-        } else {
-            console.log("Houve um erro ao tentar Lista");
-            resposta.text().then((texto) => {
-                console.error(texto);
-            });
+                });
+            } else {
+                console.log("Houve um erro ao tentar Lista");
+                resposta.text().then((texto) => {
+                    console.error(texto);
+                });
+            }
+        })
+        .catch(function (erro) {
+            console.log(erro);
+        });
+
+
+}
+listarFuncionarios()
+
+function limparLista() {
+    listaAtivosDiv.innerHTML = "";
+    listaInativosDiv.innerHTML = ""
+}
+
+function listarFuncionariosInativos() {
+    var empresa = sessionStorage.FK_EMPRESA;
+    var funcionarios;
+
+    fetch(`/gerenciadorUsuario/listarFuncionariosInativos/${empresa}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then(function (resposta) {
+            console.log("ESTOU NO THEN DO listar()!");
+
+            if (resposta.ok) {
+                resposta.json().then((json) => {
+
+                    funcionarios = json;
+                    for (var i = 0; i < funcionarios.length;) {
+
+                        if (funcionarios[i].fkSupervisor == null) {
+
+
+
+                        } else {
+
+                            listaInativosDiv.innerHTML += `<div class="item"> <span class="nomeItemLista">Nome: ${funcionarios[i].nome}</span> <span class="emailItemLista">Email: ${funcionarios[i].email}</span> <span
+                        class="telefoneItemLista"> Telefone: ${funcionarios[i].telefone}  </span>
+                   `
+                        }
+                        i++;
+                    }
+                });
+            } else {
+                console.log("Houve um erro ao tentar Lista");
+                resposta.text().then((texto) => {
+                    console.error(texto);
+                });
+            }
+        })
+        .catch(function (erro) {
+            console.log(erro);
+        });
+
+
+}
+listarFuncionariosInativos()
+
+
+
+
+
+async function deleteFuncionario(idFuncionario) {
+
+    const { value: password } = await Swal.fire({
+        title: 'Digite a sua senha',
+        input: 'password',
+        inputLabel: 'Senha supervisor',
+        inputPlaceholder: '',
+        inputAttributes: {
+            autocapitalize: 'off',
+            autocorrect: 'off'
         }
     })
-    .catch(function (erro) {
-        console.log(erro);
-    });
+
+    if (password == sessionStorage.SENHA_USUARIO) {
+        fetch(`/gerenciadorUsuario/deletar/funcionario/${idFuncionario}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then(function (resposta) {
+                console.log("ESTOU NO THEN DO listar()!");
+
+                if (resposta.ok) {
+                    resposta.json().then((json) => {
+                        limparLista()
+                        listarFuncionarios()
+                        listarFuncionariosInativos()
+                    });
+                } else {
+                    console.log("Houve um erro ao tentar Lista");
+                    resposta.text().then((texto) => {
+                        console.error(texto);
+                    });
+                }
+            })
+            .catch(function (erro) {
+                console.log(erro);
+            });
+        Swal.fire(`Usuario selecionado não possue mais acesso`)
+    } else {
+        Swal.fire(`Senha Invalida`)
+    }
 
 
+
+}
 // for (var i = 0; i < funcionarios.length; i++) {
 
 //     console.log(funcionarios[i] + "testeyhstggfd");
